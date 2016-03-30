4fbd43a192fba4319d46204081f1aad870394d88
mlucile <marie-lucile.caniard@edu.univ-fcomte.fr>
2016-03-30 20:04:43 +0200

    Classe Fou commencée V1.0

--- src/Fou.java
+++ src/Fou.java
@@ -1,7 +1,20 @@
+import javax.swing.*;
+
 /**
- * Created by cladlink on 30/03/16.
+ * Created by mlucile on 30/03/16.
  */
 public class Fou extends Piece
 {
+    private boolean noir;
+    private boolean vivant;
+    private int valeur;
+    private ImageIcon image;
+    private Coordonnees coordonnees;
 
+    public Fou()
+    {
+        this.noir = true;
+        this.vivant = true;
+        this.valeur = 3; // J'ai trouvé ca sur Wikipédia
+    }
 }
