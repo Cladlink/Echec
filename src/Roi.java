b93dfa9c55e6d970f51ac4375f74e537d4cbabb1
cladlink <michael.boutboul87@gmail.com>
2016-03-30 17:15:05 +0200

    Squelette du jeu

---
+++ src/Roi.java
@@ -0,0 +1,7 @@
+/**
+ * Created by cladlink on 30/03/16.
+ */
+public class Roi
+{
+
+}
---
+++ src/Vue.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+
+public class Vue extends JFrame
+{
+    public Vue(Model model)
+    {
+        initAttribut();
+        creerWidget();
+
+        setTitle("Chess");
+        setSize(600, 600);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+    }
+
+
+    public void initAttribut()
+    {
+
+    }
+
+
+    public void creerWidget()
+    {
+
+    }
+}
