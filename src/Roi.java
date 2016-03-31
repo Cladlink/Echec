61e06595f4f0c2c2759bb191600b90f405173a94
Sylvain <sylvain.guyot5@gmail.com>
2016-03-31 18:39:55 +0200

    roi v1.2

--- src/Roi.java
+++ src/Roi.java
@@ -7,7 +7,7 @@ public class Roi extends Piece
 {
     private final String adressePieceNoire = "adresseNoire";
     private final String adressePieceBlanche = "adresseBlanche";
-    //lol
+    //lol je suis con je push pas
 
     public Roi(int x, int y, boolean isBlanc){
         super(x,y);
