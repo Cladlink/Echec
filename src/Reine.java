7b681f47f2030b4b982b5115ef9f25df0c92a785
Gabriel <Gabriel.mercier.tw@gmail.com>
2016-03-31 13:18:05 +0200

    Classe Reine Ver 1.0

--- src/Reine.java
+++ src/Reine.java
@@ -1,7 +1,52 @@
+import javax.swing.*;
+
 /**
  * Created by cladlink on 30/03/16.
  */
 public class Reine extends Piece
 {
+    private ImageIcon noire;
+    private ImageIcon blanche;
+    private Point coordonnees;
 
+    public Reine(){
+        super();
+
+    }
+
+    public void verticalN(int nbCases){
+        this.coordonnees.setY(coordonnees.getY()+nbCases);
+    }
+
+    public void verticalS(int nbCases){
+        this.coordonnees.setY(coordonnees.getY()-nbCases);
+    }
+
+    public void horizontalE(int nbCases){
+        this.coordonnees.setX(coordonnees.getX()+nbCases);
+    }
+
+    public void horizontalO(int nbCases){
+        this.coordonnees.setX(coordonnees.getX()-nbCases);
+    }
+
+    public void diagoNE(int nbCases){
+        this.coordonnees.setY(coordonnees.getY()+nbCases);
+        this.coordonnees.setX(coordonnees.getX()+nbCases);
+    }
+
+    public void diagoNO(int nbCases){
+        this.coordonnees.setY(coordonnees.getY()+nbCases);
+        this.coordonnees.setX(coordonnees.getX()-nbCases);
+    }
+
+    public void diagoSE(int nbCases){
+        this.coordonnees.setY(coordonnees.getY()-nbCases);
+        this.coordonnees.setX(coordonnees.getX()+nbCases);
+    }
+
+    public void diagoSO(int nbCases){
+        this.coordonnees.setY(coordonnees.getY()-nbCases);
+        this.coordonnees.setX(coordonnees.getX()-nbCases);
+    }
 }
