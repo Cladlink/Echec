3145fbc16d5e51942c33f03a526a3a78eb1e1c2d
cladlink <michael.boutboul87@gmail.com>
2016-03-31 09:31:56 +0200

    Squelette du jeu

--- src/Piece.java
+++ src/Piece.java
@@ -1,12 +1,14 @@
-/**
+/*
  * Created by cladlink on 30/03/16.
  */
 public class Piece
 {
     protected boolean vivant;
+    protected Point coordonnees;
 
-    public Piece()
+    public Piece(int x, int y)
     {
         this.vivant = true;
+        this.coordonnees = new Point(x, y);
     }
 }
