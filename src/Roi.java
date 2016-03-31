fac0e97b38faa9320dd4216686a32f46d1a8f777
Sylvain <sylvain.guyot5@gmail.com>
2016-03-31 18:23:56 +0200

    roi v1.2

--- src/Roi.java
+++ src/Roi.java
@@ -1,7 +1,24 @@
+import javax.swing.*;
+
 /*
  * Created by cladlink on 30/03/16.
  */
 public class Roi extends Piece
 {
+    private ImageIcon blanche;
+    private ImageIcon noire;
+    private Point coordonees;
+
+    // Todo plus tard
+    public Roi(){
+        super();
+
+    }
+
+    public void verticalN(){ this.coordonees.setY(coordonees.getY()+1); }
+    public void verticalS(){ this.coordonees.setY(coordonees.getY()-1); }
+    public void horizontalO() { this.coordonees.setX(coordonees.getX()-1); }
+    public void horizontalE() { this.coordonees.setX(coordonees.getX()+1); }
+
 
 }
