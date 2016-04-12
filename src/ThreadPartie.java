ea09a0ffb80cc24443961f6e0fc001a0a8c5e44e
cladlink <michael.boutboul87@gmail.com>
2016-04-12 21:09:58 +0200

    Canevas V2.0 (a priori le dernier !) + pièces

--- src/ThreadPartie.java
+++ src/ThreadPartie.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,50 +13,60 @@ public class ThreadPartie extends Thread
     private ServerSocket conn = null;
     private Socket comm = null;
     private Control controller = null;
+    private int port;
 
-    public ThreadPartie(Partie partie, Boolean isServer, ControlButton controller)
+    public ThreadPartie(Partie partie, Boolean isServer, ControlButton controller, int port)
     {
         this.partie = partie;
         this.isServer = isServer;
         this.controller = controller;
+        this.port = port;
     }
 
     @Override
     public void run()
     {
-        if(isServer)
+        if(isServer)// todo y a pas un soucis à faire comme ca ? on va pas réinitialiser la connection à chaque fois ? si ?
         {
-            // créer conn + attendre connexion
+            try
+            {
+                conn = new ServerSocket(port);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
         }
         else
         {
-            // créer comm
+            comm = new Socket();
         }
+
         // créer les objets flux
         // échanger les infos (pseudos, qui joue en premier, ...)
-        boolean myturn;
-        if (true)// mettre ici le teste si il y a une réponse du serveur
+        boolean myTurn;
+        if (true)// todo mettre ici le teste si il y a une réponse du serveur
         {
             this.moi = partie.getJoueurBlanc();
-            myturn = true;
+            myTurn = true;
         }
         else
         {
             this.moi = partie.getJoueurNoir();
-            myturn = false;
+            myTurn = false;
         }
         boolean stop = false;
         partie.attenteDebutPartie();
         while(!stop)
         {
-            if (myturn)
+            if (myTurn)
             {
                 partie.attenteAction();// j'attends que le joueur ai joué;
                 // envoyer à l'autre le coup joué
                 if (partie.isFinPartie())
                     stop = true;
                 else
-                    myturn = false;
+                    myTurn = false;
             }
             else
             {
@@ -65,7 +76,7 @@ public class ThreadPartie extends Thread
                 if (partie.isFinPartie())
                     stop = true;
                 else
-                    myturn = false;
+                    myTurn = false;
             }
         }
     }
